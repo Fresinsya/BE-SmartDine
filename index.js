ad1d85cf59c6452ea82decf79b42a43887285d28
Fresinsya <vicafresinsya@gmail.com>
2024-02-12 15:57:54 +0700

    3. sambung ke database di mongo

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const mongoose = require("./config/mongoo")
 
 const app = express();
 
+mongoose.then(()=>{
+    console.log("mongoose berhasil")
+})
+.catch(()=>{
+    console.log("mongoose gagal")
+})
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
