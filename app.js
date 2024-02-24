8add55eeace2696b8c36d34d239dd95240bc395e
Fresinsya <vicafresinsya@gmail.com>
2024-02-24 15:25:17 +0700

    tambah limit

--- app.js
+++ app.js
@@ -14,9 +14,11 @@ mongoose.then(() => {
         console.log("mongoose gagal")
     })
 app.use(cors());
-app.use(express.json());
+app.use(express.json({
+    limit: "50mb",
+}));
 app.use(bodyParser.json({
-    limit : 5000
+    limit: "50mb",
 }));
 
 app.use(route)
