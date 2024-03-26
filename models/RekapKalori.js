745b75775c6793e177ad7ad89d37ff7ad279e7f1
Fresinsya <vicafresinsya@gmail.com>
2024-03-26 21:24:35 +0700

    rekap

--- models/RekapKalori.js
+++ models/RekapKalori.js
@@ -1,9 +1,17 @@
 const mongoose = require('mongoose');
 
 const RekapKaloriSchema = new mongoose.Schema({
-    tgl: {
+    tgl_input: {
         type: Date,
         default: Date.now
+    },
+    tgl_selesai: {
+        type: Date,
+        default: function() {
+            const sevenDaysLater = new Date();
+            sevenDaysLater.setDate(this.tgl_input.getDate() + 7);
+            return sevenDaysLater;
+        }
     },
     IdUser: {
         type: mongoose.Schema.Types.ObjectId,
--- routes/user-route.js
+++ routes/user-route.js
@@ -22,11 +22,89 @@ route.post("/process-data/:id", async (req, res) => {
 
   // Memanggil skrip Python dengan PythonShell
   try {
-
     const riwayat = await Riwayat.findOne({ IdUser: id });
-
-    // Jika riwayat tidak ditemukan, lakukan pengolahan data
     if (!riwayat) {
+      return res.status(404).json({
+        status: "Error",
+        message: "Riwayat tidak ditemukan",
+      });
+    }
+
+    // Lakukan pengecekan data user
+    if (!riwayat.FACV || !riwayat.FCVC || !riwayat.NCP || !riwayat.CAEC || !riwayat.CH20 || !riwayat.SCC || !riwayat.FAF || !riwayat.TUE || !riwayat.CALC || !riwayat.MTRANS) {
+      return res.status(400).json({
+        status: "Error",
+        message: "Data Riwayat tidak lengkap",
+      });
+    }
+
+    const rekap = await RekapKalori.findOne({ IdUser: id, tgl_selesai: { $gt: new Date() } }).sort({ tgl_selesai: 1 });
+    if (rekap) {
+
+      const today = new Date();
+      const tglSelesai = rekap.tgl_selesai;
+
+      if (today > tglSelesai || today === tglSelesai) {
+        // Tgl_selesai sudah terlampaui, jalankan proses yang sama seperti ketika riwayat tidak ditemukan
+        console.log('Tanggal selesai sudah terlampaui:', today, '>', tglSelesai)
+        let objectId = typeof (id);
+        console.log('ObjectId yang valid:', objectId);
+        const messages = await PythonShell.run('controllers/knn.py', options);
+        const processedData = messages.toString();
+        console.log("Processed Data:", processedData);
+
+        const Data = JSON.parse(messages[0]);
+        const kalori = Data["Kalori Harian"].replace(",", "");
+        console.log("Kalori:", kalori);
+        const user = await User.findByIdAndUpdate(id, {
+          ...req.body,
+          kaloriHarian: kalori,
+        }, { new: true });
+
+        const BMR = Data["BMR"].replace(",", "");
+        const TDEE = Data["TDEE"].replace(",", "");
+        const NObeyesdad = Data["NObeyesdad"];
+        console.log("BMR:", BMR);
+        console.log("TDEE:", TDEE);
+        console.log("NObeyesdad:", NObeyesdad);
+        const riwayat = await Riwayat.findOneAndUpdate({ IdUser: id }, {
+          ...req.body,
+          BMR: BMR,
+          TDEE: TDEE,
+          NObeyesdad: NObeyesdad,
+        });
+
+        const Defisit = Data["Defisit Kalori"].replace(",", "");
+        let rekapKalori;
+
+        rekapKalori = await RekapKalori.create({
+          IdUser: id,
+          total_kalori_harian: kalori,
+          BMR: BMR,
+          TDEE: TDEE,
+          Defisit: Defisit,
+        });
+        console.log('Rekap kalori berhasil dibuat:', rekapKalori);
+
+        // if (!user || !riwayat) {
+        //   return res.status(404).json({
+        //     message: "id tidak ditemukan",
+        //   });
+        // } else {
+        res.status(200).json({
+          status: "oke",
+          message: "berhasil mengubah data",
+          data: rekapKalori, // Gunakan nilai rekapKalori yang didefinisikan di dalam blok try-catch
+        });
+        // }
+      } else {
+        // Tgl_selesai belum terlampaui, kirimkan pesan ke client
+        res.status(400).json({
+          status: "Error",
+          message: "Proses tidak dapat dilakukan karena tgl_selesai belum terlampaui",
+        });
+      }
+    } else {
       let objectId = typeof (id);
       console.log('ObjectId yang valid:', objectId);
       const messages = await PythonShell.run('controllers/knn.py', options);
@@ -34,27 +112,27 @@ route.post("/process-data/:id", async (req, res) => {
       console.log("Processed Data:", processedData);
 
       const Data = JSON.parse(messages[0]);
-      const kalori = (Data["Kalori Harian"].replace(",", ""));
+      const kalori = Data["Kalori Harian"].replace(",", "");
       console.log("Kalori:", kalori);
       const user = await User.findByIdAndUpdate(id, {
         ...req.body,
         kaloriHarian: kalori,
       }, { new: true });
 
-      const BMR = (Data["BMR"].replace(",", ""));
-      const TDEE = (Data["TDEE"].replace(",", ""));
-      const NObeyesdad = (Data["NObeyesdad"]);
+      const BMR = Data["BMR"].replace(",", "");
+      const TDEE = Data["TDEE"].replace(",", "");
+      const NObeyesdad = Data["NObeyesdad"];
       console.log("BMR:", BMR);
       console.log("TDEE:", TDEE);
       console.log("NObeyesdad:", NObeyesdad);
-      const newRiwayat = await Riwayat.findOneAndUpdate({ IdUser: id }, {
+      const riwayat = await Riwayat.findOneAndUpdate({ IdUser: id }, {
         ...req.body,
         BMR: BMR,
         TDEE: TDEE,
         NObeyesdad: NObeyesdad,
-      }, { upsert: true, new: true });
+      });
 
-      const Defisit = (Data["Defisit Kalori"].replace(",", ""));
+      const Defisit = Data["Defisit Kalori"].replace(",", "");
       let rekapKalori;
 
       rekapKalori = await RekapKalori.create({
@@ -68,75 +146,9 @@ route.post("/process-data/:id", async (req, res) => {
 
       res.status(200).json({
         status: "oke",
-        message: "berhasil mengubah data",
-        data: rekapKalori,
+        message: "berhasil menambahkan data",
+        data: rekapKalori, // Gunakan nilai rekapKalori yang didefinisikan di dalam blok try-catch
       });
-    } else {
-      // Jika riwayat ditemukan, cek tgl_selesai
-      const today = new Date();
-      const tglSelesai = new Date(riwayat.tgl_selesai);
-      if (today.getFullYear() > tglSelesai.getFullYear() ||
-        (today.getFullYear() === tglSelesai.getFullYear() && today.getMonth() > tglSelesai.getMonth()) ||
-        (today.getFullYear() === tglSelesai.getFullYear() && today.getMonth() === tglSelesai.getMonth() && today.getDate() > tglSelesai.getDate())) {
-        // Tgl_selesai belum terlampaui, kirim respon error
-        return res.status(400).json({
-          message: "Tgl_selesai belum terlampaui",
-        });
-      } else {
-        // Tgl_selesai sudah terlampaui, jalankan proses yang sama seperti ketika riwayat tidak ditemukan
-        let objectId = typeof (id);
-        console.log('ObjectId yang valid:', objectId);
-        const messages = await PythonShell.run('controllers/knn.py', options)
-        const processedData = messages.toString();
-        console.log("Processed Data:", processedData);
-
-        const Data = JSON.parse(messages[0]);
-        const kalori = (Data["Kalori Harian"].replace(",", ""));
-        console.log("Kalori:", kalori);
-        const user = await User.findByIdAndUpdate(id, { // Perbaikan penggunaan id
-          ...req.body,
-          kaloriHarian: kalori,
-        }, { new: true });
-
-
-        const BMR = (Data["BMR"].replace(",", ""));
-        const TDEE = (Data["TDEE"].replace(",", ""));
-        const NObeyesdad = (Data["NObeyesdad"]);
-        console.log("BMR:", BMR);
-        console.log("TDEE:", TDEE);
-        console.log("NObeyesdad:", NObeyesdad);
-        const riwayat = await Riwayat.findOneAndUpdate({ IdUser: id }, {
-          ...req.body,
-          BMR: BMR,
-          TDEE: TDEE,
-          NObeyesdad: NObeyesdad,
-        });
-
-        const Defisit = (Data["Defisit Kalori"].replace(",", ""));
-        let rekapKalori; // Definisikan variabel rekapKalori di dalam blok if
-
-        // Cek apakah perbedaan waktu sudah mencapai 1 hari
-        rekapKalori = await RekapKalori.create({
-          IdUser: id,
-          total_kalori_harian: kalori,
-          BMR: BMR,
-          TDEE: TDEE,
-          Defisit: Defisit,
-        });
-        console.log('Rekap kalori berhasil dibuat:', rekapKalori);
-
-        if (!user || !riwayat) {
-          return res.status(404).json({
-            message: "id tidak ditemukan",
-          });
-        } else {
-          res.status(200).json({
-            status: "oke",
-            message: "berhasil mengubah data",
-            data: rekapKalori, // Gunakan nilai rekapKalori yang didefinisikan di dalam blok try-catch
-          });
-        }
-      }
     }
   } catch (err) {
     console.error("Error:", err);
